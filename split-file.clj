5e7ba2cd8b06a030a9ba2217a3344bc6a6c6667b
Daniel Medeiros <daniel@arneam.com>
2019-09-27 05:34:42 -0300

    Refactor aiming best practices in code

--- split-file.clj
+++ split-file.clj
@@ -3,26 +3,24 @@
 (use '[clojure.string :only [split-lines join]])
 
 (defn- partitioned-data-from-file
-			 "Load a file and return it as a partitioned list."
-			 [file pieces]
-			 (let [data (split-lines (slurp file))]
-						(partition-all (/ (count data) pieces) data)))
+  "Load a file and return it as a partitioned list."
+  [file pieces]
+  (let [data (split-lines (slurp file))]
+    (partition-all (/ (count data) pieces) data)))
 
 (defn- generate-file
-			 "Create partial file based on index."
-			 [file i partition-data]
-			 (let [file-name (str file "." i)]
-						(printf "generating: %s\n" file-name)
-						(spit file-name (join "\n" (nth partition-data i)))))
+  "Create partial file based on index."
+  [file i partition-data]
+  (let [file-name (str file "." i)]
+    (printf "generating: %s\n" file-name)
+    (spit file-name (join "\n" (nth partition-data i)))))
 
 (defn split-file
-			"Split a given file in several files.
-       Generates several files with standard sequential
-       numbering, starting at zero.
-       Created for parallel processing of files."
-			[file pieces]
-			(let [partition-data (partitioned-data-from-file file pieces)]
-					 (loop [i 0]
-								 (when (< i (count partition-data))
-											 (generate-file file i partition-data)
-											 (recur (inc i))))))
+  "Split a given file in several files.
+   Generates several files with standard sequential
+   numbering, starting at zero.
+   Created for parallel processing of files."
+  [file pieces]
+  (let [partition-data (partitioned-data-from-file file pieces)]
+    (map #(generate-file file % partition-data)
+          (range 0 (count partition-data)))))
