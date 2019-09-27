83f580d4d43eaeb1d73ada5cd6da27422d61bb1b
Daniel Medeiros <daniel@arneam.com>
2019-09-27 05:31:18 -0300

    Split unique function in smaller, more specialized functions

--- split-file.clj
+++ split-file.clj
@@ -1,13 +1,28 @@
+;; https://github.com/bbatsov/clojure-style-guide
+
+(use '[clojure.string :only [split-lines join]])
+
+(defn- partitioned-data-from-file
+			 "Load a file and return it as a partitioned list."
+			 [file pieces]
+			 (let [data (split-lines (slurp file))]
+						(partition-all (/ (count data) pieces) data)))
+
+(defn- generate-file
+			 "Create partial file based on index."
+			 [file i partition-data]
+			 (let [file-name (str file "." i)]
+						(printf "generating: %s\n" file-name)
+						(spit file-name (join "\n" (nth partition-data i)))))
+
 (defn split-file
-			"Split a file in several pieces, generating several files
-       with standard sequential numbering, starting at zero.
+			"Split a given file in several files.
+       Generates several files with standard sequential
+       numbering, starting at zero.
        Created for parallel processing of files."
 			[file pieces]
-			(let [data (split-lines (slurp file))
-						partition-data (partition-all (/ (count data) pieces) data)]
+			(let [partition-data (partitioned-data-from-file file pieces)]
 					 (loop [i 0]
-								 (if (< i (count partition-data))
-									 (let [file-name (str file "." i)]
-												(printf "generating: %s\n" file-name)
-												(spit file-name (join "\n" (nth partition-data i)))
-												(recur (inc i)))))))
+								 (when (< i (count partition-data))
+											 (generate-file file i partition-data)
+											 (recur (inc i))))))
